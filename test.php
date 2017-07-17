230aa03db4488a351376e95a624df4b929f550c5
guopeng08 <18224577619@163.com>
2017-07-17 11:19:04 +0800

    php问题修改

--- test.php
+++ test.php
@@ -19,8 +19,34 @@
  $b = new liyongzhen('3333');
  // var_dump($b);
  $c = $b->test();
- // echo $c;
 
+/**
+ * 获取数组中最小的K个数
+ */
+
+function replaceMaxValue($arr,$otherNum){
+    $max = $arr[0];
+    for($i = 1; $i < count($arr); $i++){
+        if($arr[$i] > $max){
+            $max = &$arr[$i];
+        }
+    }
+    if($otherNum < $max){
+        $max = $otherNum;
+    }
+    return $arr;
+}
+
+function getTop($arr, $topNum){
+    $topK = array_slice($arr, 0, $topNum);
+    $other = array_slice($arr,$topNum);
+    foreach($other as $value){
+        $topK = replaceMaxValue($topK,$value);
+    }
+    return $topK;
+}
+
+print_r(getTop(array(1,2,5,6,7,976,0,4,123,4,5),6));
 
 
 
